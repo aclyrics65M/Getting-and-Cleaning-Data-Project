8f5aa129cab1432d64657f98f1c28daf9fe95181
aclyrics65M <fgmamagh315@gmail.com>
2017-06-25 16:31:14 -0400

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,4 @@
+setwd("C:/Users/Faraz/Desktop/Coursera Data Science Course Sequences/Course 03 Getting and Cleaning Data/getdata%2Fprojectfiles%2FUCI HAR Dataset/UCI HAR Dataset")
+
+library(plyr)
+library(data.table)
